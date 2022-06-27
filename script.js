a5f69759a9831729792bbeab789fbfb57e719f54
Karl Oskar Kuuse <oskarkuuse@gmail.com>
2022-06-27 15:09:04 +0300

    movement logic initial version complete - some test jquery

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let board = [[2, 2, 2, 2],
+let board = [[0, 0, 0, 0],
+             [2, 0, 0, 0],
              [0, 0, 0, 0],
-             [2, 0, 0, 2],
-             [8, 4, 4, 8]]
+             [0, 2, 0, 0]]
 
 function updateGameBoardCells(gameBoardInfo) {
     let table = document.getElementById("gameBoardTable");
@@ -25,12 +25,14 @@ function moveTilesRight(gameBoardInfo) {
                 gameBoardInfo[i][j - 1] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
                 j--;
-            } else if (j - 2 >= 0 && gameBoardInfo[j - 2] !== 0 && gameBoardInfo[i][j - 1] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j - 2]) {
+            } else if (j - 2 >= 0 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j - 1] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j - 2]) {
                 gameBoardInfo[i][j - 2] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
-            } else if (j - 3 >= 0 && gameBoardInfo[j - 3] !== 0 && gameBoardInfo[i][j - 2] === 0 && gameBoardInfo[i][j - 1] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j - 3]) {
+                j -= 2;
+            } else if (j - 3 >= 0 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j - 2] === 0 && gameBoardInfo[i][j - 1] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j - 3]) {
                 gameBoardInfo[i][j - 3] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
+                j -= 3;
             }
         }
 
@@ -50,16 +52,18 @@ function moveTilesRight(gameBoardInfo) {
 function moveTilesLeft(gameBoardInfo) {
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 3; j++) {
-            if (gameBoardInfo[i][j] === gameBoardInfo[i][j + 1]) {
+            if (gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j + 1]) {
                 gameBoardInfo[i][j + 1] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
                 j++;
-            } else if (j + 2 <= 3 && gameBoardInfo[j + 2] !== 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j + 2]) {
+            } else if (j + 2 <= 3 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j + 1] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j + 2]) {
                 gameBoardInfo[i][j + 2] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
-            } else if (j + 3 <= 3 && gameBoardInfo[j + 3] !== 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j + 3]) {
+                j += 2;
+            } else if (j + 3 <= 3 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j + 1] === 0 && gameBoardInfo[i][j + 2] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i][j + 3]) {
                 gameBoardInfo[i][j + 3] = gameBoardInfo[i][j] * 2;
                 gameBoardInfo[i][j] = 0;
+                j += 3;
             }
         }
 
@@ -76,6 +80,76 @@ function moveTilesLeft(gameBoardInfo) {
     updateGameBoardCells(gameBoardInfo);
 }
 
+function moveTilesUp(gameBoardInfo) {
+    for (let j = 0; j < 4; j++) {
+        for (let i = 0; i < 3; i++) {
+            if (gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j] === gameBoardInfo[i + 1][j]) {
+                gameBoardInfo[i + 1][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i++;
+            } else if (i + 2 <= 3 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i + 1][j] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i + 2][j]) {
+                gameBoardInfo[i + 2][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i += 2;
+            } else if (i + 3 <= 3 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i + 1][j] === 0 && gameBoardInfo[i + 2][j] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i + 3][j]) {
+                gameBoardInfo[i + 3][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i += 3;
+            }
+        }
+
+        let newColumn = [0, 0, 0, 0];
+        let k = 0;
+        for (let i = 0; i <= 3; i++) {
+            if (gameBoardInfo[i][j] !== 0) {
+                newColumn[k] = gameBoardInfo[i][j];
+                k++;
+            }
+        }
+
+        for (let i = 0; i <= 3; i++) {
+            gameBoardInfo[i][j] = newColumn[i];
+        }
+
+    }
+    updateGameBoardCells(gameBoardInfo);
+}
+
+function moveTilesDown(gameBoardInfo) {
+    for (let j = 0; j < 4; j++) {
+        for (let i = 3; i > 0; i--) {
+            if (gameBoardInfo[i][j] !== 0 && gameBoardInfo[i][j] === gameBoardInfo[i - 1][j]) {
+                gameBoardInfo[i - 1][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i--;
+            } else if (i - 2 >= 0 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i - 1][j] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i - 2][j]) {
+                gameBoardInfo[i - 2][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i -= 2;
+            } else if (i - 3 >= 0 && gameBoardInfo[i][j] !== 0 && gameBoardInfo[i - 1][j] === 0 && gameBoardInfo[i - 2][j] === 0 && gameBoardInfo[i][j] === gameBoardInfo[i - 3][j]) {
+                gameBoardInfo[i - 3][j] = gameBoardInfo[i][j] * 2;
+                gameBoardInfo[i][j] = 0;
+                i -= 3;
+            }
+        }
+
+        let newColumn = [0, 0, 0, 0];
+        let k = 3;
+        for (let i = 3; i >= 0; i--) {
+            if (gameBoardInfo[i][j] !== 0) {
+                newColumn[k] = gameBoardInfo[i][j];
+                k--;
+            }
+        }
+
+        for (let i = 3; i >= 0; i--) {
+            gameBoardInfo[i][j] = newColumn[i];
+        }
+
+    }
+    updateGameBoardCells(gameBoardInfo);
+}
+
 function copyBoard(gameBoardInfo) {
     let newBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
     for (let i = 0; i < 4; i++) {
@@ -86,17 +160,27 @@ function copyBoard(gameBoardInfo) {
     return newBoard;
 }
 
+// Jquery demo
+// $(".gameBoard").click(function(){
+//     // $("td").css({position: "relative"})
+//     $(".gameBoardCell").css({position: "relative"}).animate({position: "relative", opacity: 0.25, left: "124px"});
+// });
+
 updateGameBoardCells(board)
-
-// moveTilesRight(board)
-
-// document.addEventListener("click", function() {moveTilesRight(board)})
 
 document.addEventListener("keydown", (event) => {
     if (event.code === "ArrowRight") {
         moveTilesRight(board)
     } else if (event.code === "ArrowLeft") {
         moveTilesLeft(board)
+    } else if (event.code === "ArrowUp") {
+        moveTilesUp(board)
+    } else if (event.code === "ArrowDown") {
+        moveTilesDown(board)
+    } else if (event.code === "KeyR") {
+        let info = prompt("rida veerg")
+        let numbers = info.split(" ")
+        board[parseInt(numbers[0]) - 1][parseInt(numbers[1]) - 1] = 2
+        updateGameBoardCells(board)
     }
 })
-
